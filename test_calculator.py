42bebd032b81a6569db6746a342d49ff9ac502fc
unknown <prathmeshsandikar@gmail.com>
2022-03-25 15:14:02 +0530

    Testing using setUp and tearDown

--- test_calculator.py
+++ test_calculator.py
@@ -3,16 +3,43 @@ import Calculator
 import unittest
 
 class TestCalculator(unittest.TestCase):
+
+    def setUp(self):
+        self.x=34
+        self.y=30
+
+    def tearDown(self):
+        self.x=0
+        self.y=0
+
+
     def test_add(self):
-        x=10
-        y=20
-        result=Calculator.add(x,y)
-        self.assertEqual(x+y,result)
+        # x=10
+        # y=20
+        result=Calculator.add(self.x,self.y)
+        self.assertEqual(self.x+self.y,result)
+
+
 
     def test_sub(self):
-        x=20
-        y=10
-        result=Calculator.sub(x,y)
-        self.assertEqual(x-y,result)
+        #ARRANGE
+        # x=20
+        # y=10
+        #ACT
+        result=Calculator.sub(self.x,self.y)
+        #ASSERT
+        self.assertEqual(self.x-self.y,result)
+
+    def test_mult(self):
+        # x=5
+        # y=5
+        result=Calculator.mult(self.x,self.y)
+        self.assertEqual(self.x*self.y,result)
+
+    def test_div(self):
+        # x=10
+        # y=2
+        result=Calculator.div(self.x,self.y)
+        self.assertEqual(self.x/self.y,result)
 if __name__=="__main__":
     unittest.main()
